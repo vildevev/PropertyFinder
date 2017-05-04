b1bd248a2d4bd7f10ac2aefd0e2f42f57157dacd
“Vilde <vilde@vevatne.no>
2017-05-03 20:02:07 -0700

    Adds hello world to app

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+'use strict';
+var React = require('react');
+var ReactNative = require('react-native');
+
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+var PropertyFinderApp extends React.Component {
+  render() {
+    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
+  }
+}
+
+ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp});
