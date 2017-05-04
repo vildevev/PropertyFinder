f7e1d1ca9c31a3a42190129a393a95074b0ce058
“Vilde <vilde@vevatne.no>
2017-05-03 20:53:21 -0700

    Adds navbar

--- index.ios.js
+++ index.ios.js
@@ -8,15 +8,29 @@ var styles = ReactNative.StyleSheet.create({
     backgroundColor: 'white',
     fontSize: 30,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
-class PropertyFinderApp extends React.Component {
+class HelloWorld extends React.Component {
   render() {
-    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
+    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
   }
 }
-
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: HelloWorld,
+        }}/>
+    );
+  }
+}
 
 
 ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
